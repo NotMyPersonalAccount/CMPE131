f6f8c9cdbc3bc6ea5adbf5020a59763ad885a5af
NotMyPersonalAccount <kevinzj710@gmail.com>
2024-03-25 20:37:11 -0700

    Create basic home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,25 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
-	return <></>;
+	return (
+		<>
+			<section className="flex flex-col px-6 py-24 items-center gap-6">
+				<div className="flex flex-col gap-2 items-center text-center">
+					<h1 className="text-3xl sm:text-6xl font-bold">
+						Create and share recipes
+					</h1>
+					<span className="text-muted-foreground text-lg sm:text-xl max-w-[250px] sm:max-w-[600px]">
+						Join a community of people in creating and sharing recipes. Find new
+						recipes to try out. Explore new tastes and new cuisines.
+					</span>
+				</div>
+				<div>
+					<Button asChild>
+						<Link href="/browse">Join Today</Link>
+					</Button>
+				</div>
+			</section>
+		</>
+	);
 }
