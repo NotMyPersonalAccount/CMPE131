57d2e0017f2f2999b3c3ee67a980c804d3de1a28
NotMyPersonalAccount <kevinzj710@gmail.com>
2024-03-26 00:45:05 -0700

    Hide hydration warning on html caused by theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
+		<html lang="en" suppressHydrationWarning>
 			<UserProvider>
 				<body
 					className={cn("min-h-screen bg-background font-inter antialiased")}
