7516734799c43a8e254849e3beefbcc60aaffa6c
NotMyPersonalAccount <kevinzj710@gmail.com>
2024-04-17 21:24:16 -0700

    Add recipe page skeleton

--- src/app/(authenticated)/recipe/[recipe]/page.tsx
+++ src/app/(authenticated)/recipe/[recipe]/page.tsx
@@ -4,41 +4,69 @@ import { notFound } from "next/navigation";
 import CommentSection, { CommentBox } from "./CommentSection";
 import { Separator } from "@/components/ui/separator";
 import BackButton from "./BackButton";
-import { Recipe } from "@prisma/client";
 import { Suspense } from "react";
 import { LoaderIcon } from "lucide-react";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default async function RecipePage({
 	params,
 }: {
 	params: { recipe: string };
 }) {
-	const recipe = await prisma.recipe.findUnique({
-		where: {
-			id: params.recipe,
-		},
-	});
-	if (!recipe) return notFound();
-
 	return (
 		<>
 			<BackButton />
 			<section className="py-16 px-8 md:px-16 xl:px-32 flex flex-col gap-2 sm:gap-4">
-				<div>
-					<h1 className="text-2xl sm:text-4xl font-bold">{recipe.title}</h1>
-					<h2 className="text-md sm:text-lg text-muted-foreground">
-						{recipe.description}
-					</h2>
-				</div>
-				<div className="bg-gray-200 w-full h-40 sm:h-80 lg:h-96 rounded-md"></div>
-				<Card className="min-h-40">
-					<CardContent className="py-4">{recipe.content}</CardContent>
-				</Card>
-				<Separator className="my-8" />
-				<Suspense fallback={<CommentsSkeleton />}>
-					<Comments recipeId={params.recipe} />
+				<Suspense fallback={<RecipeSkeleton />}>
+					<Recipe id={params.recipe} />
+					<Separator className="my-8" />
+					<Suspense fallback={<CommentsSkeleton />}>
+						<Comments recipeId={params.recipe} />
+					</Suspense>
 				</Suspense>
 			</section>
+		</>
+	);
+}
+
+async function Recipe({ id }: { id: string }) {
+	const recipe = await prisma.recipe
+		.findUnique({
+			where: {
+				id,
+			},
+		})
+		.catch(() => null);
+
+	if (!recipe) return notFound();
+
+	return (
+		<>
+			<div>
+				<h1 className="text-2xl sm:text-4xl font-bold">{recipe.title}</h1>
+				<h2 className="text-md sm:text-lg text-muted-foreground">
+					{recipe.description}
+				</h2>
+			</div>
+			<div className="bg-gray-200 w-full h-40 sm:h-80 lg:h-96 rounded-md"></div>
+			<Card className="min-h-40">
+				<CardContent className="py-4">{recipe.content}</CardContent>
+			</Card>
+		</>
+	);
+}
+
+function RecipeSkeleton() {
+	return (
+		<>
+			<div>
+				<Skeleton className="sm:w-96 h-8 sm:h-10" />
+				<Skeleton className="h-6 sm:h-7 mt-1" />
+			</div>
+			<Skeleton className="h-40 sm:h-80 lg:h-96" />
+			<Skeleton className="h-40" />
+			<Separator className="my-8" />
+			<CommentsSkeleton />
 		</>
 	);
 }
