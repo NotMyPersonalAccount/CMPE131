30ffa8913919ac9b3f34084e7431487e7abad1b3
NotMyPersonalAccount <kevinzj710@gmail.com>
2024-04-17 21:03:40 -0700

    Don't wait for comments before loading

--- src/app/(authenticated)/recipe/[recipe]/CommentSection.tsx
+++ src/app/(authenticated)/recipe/[recipe]/CommentSection.tsx
@@ -68,7 +68,7 @@ export default function CommentSection({
 	);
 }
 
-function CommentBox({
+export function CommentBox({
 	onSubmit,
 	onCancel,
 	showCancel,
--- src/app/(authenticated)/recipe/[recipe]/page.tsx
+++ src/app/(authenticated)/recipe/[recipe]/page.tsx
@@ -1,9 +1,12 @@
 import { Card, CardContent } from "@/components/ui/card";
 import prisma from "@/lib/prisma";
 import { notFound } from "next/navigation";
-import CommentSection from "./CommentSection";
+import CommentSection, { CommentBox } from "./CommentSection";
 import { Separator } from "@/components/ui/separator";
 import BackButton from "./BackButton";
+import { Recipe } from "@prisma/client";
+import { Suspense } from "react";
+import { LoaderIcon } from "lucide-react";
 
 export default async function RecipePage({
 	params,
@@ -13,26 +16,6 @@ export default async function RecipePage({
 	const recipe = await prisma.recipe.findUnique({
 		where: {
 			id: params.recipe,
-		},
-		include: {
-			comments: {
-				where: {
-					parent: null,
-				},
-				include: {
-					user: true,
-					children: {
-						include: {
-							user: true,
-						},
-					},
-				},
-				orderBy: [
-					{
-						timestamp: "desc",
-					},
-				],
-			},
 		},
 	});
 	if (!recipe) return notFound();
@@ -52,11 +35,51 @@ export default async function RecipePage({
 					<CardContent className="py-4">{recipe.content}</CardContent>
 				</Card>
 				<Separator className="my-8" />
-				<div>
-					<h1 className="font-bold text-lg">Comments</h1>
-					<CommentSection recipe={recipe} initialComments={recipe.comments} />
-				</div>
+				<Suspense fallback={<CommentsSkeleton />}>
+					<Comments recipe={recipe} />
+				</Suspense>
 			</section>
 		</>
+	);
+}
+
+async function Comments({ recipe }: { recipe: Recipe }) {
+	const comments = await prisma.recipeComment.findMany({
+		where: {
+			recipeId: recipe.id,
+			parent: null,
+		},
+		include: {
+			user: true,
+			children: {
+				include: {
+					user: true,
+				},
+			},
+		},
+		orderBy: [
+			{
+				timestamp: "desc",
+			},
+		],
+	});
+
+	return (
+		<div>
+			<h1 className="font-bold text-lg">Comments</h1>
+			<CommentSection recipe={recipe} initialComments={comments} />
+		</div>
+	);
+}
+
+function CommentsSkeleton() {
+	return (
+		<div>
+			<h1 className="font-bold text-lg">Comments</h1>
+			<CommentBox showCancel={false} />
+			<div className="flex justify-center">
+				<LoaderIcon className="animate-spin" />
+			</div>
+		</div>
 	);
 }
