2253955fa5e70b77eddbf8b0dac302f1ea83db1d
NotMyPersonalAccount <kevinzj710@gmail.com>
2024-05-07 15:47:54 -0700

    Add "text" prop to SubmitButton

--- src/components/SubmitButton.tsx
+++ src/components/SubmitButton.tsx
@@ -2,14 +2,15 @@ import { ReloadIcon } from "@radix-ui/react-icons";
 import { Button, ButtonProps } from "./ui/button";
 
 interface Props extends ButtonProps {
+	text?: string;
 	submitting: boolean;
 }
 
-export default function SubmitButton({ submitting, ...props }: Props) {
+export default function SubmitButton({ text, submitting, ...props }: Props) {
 	return (
 		<Button type="submit" disabled={submitting} {...props}>
 			{submitting && <ReloadIcon className="mr-2 animate-spin" />}
-			Submit
+			{text ?? "Submit"}
 		</Button>
 	);
 }
